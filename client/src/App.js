ae21f502bb5007d9c5acddde8801dac6bbe17433
jiko <>
2020-08-16 14:50:39 +0900

    updated header in client/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
+          <h1>Fic Calculator Kubernetes!</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
